7f2a6763032186f94d86a55958a633119755ecdf
Eduardo Barros <eduardo.vfbarros@gmail.com>
2026-04-19 09:41:05 -0300

    Alteracoes e ajustes nas principais classes

--- src/entidades/NPC.java
+++ src/entidades/NPC.java
@@ -2,7 +2,7 @@ package entidades;
 
 public class NPC extends Character {
     public NPC(String nome, int vida, int stamina , int ataque , int defesa , int velocidade){
-        super(vida, stamina, ataque, defesa, velocidade);
+        super(nome, vida, stamina, ataque, defesa, velocidade);
     }
 
     @Override
--- src/entidades/Tripulante.java
+++ src/entidades/Tripulante.java
@@ -1,8 +1,10 @@
 package entidades;
 
 public class Tripulante extends Character {
+    private HabilidadeEspecial habilidadeEspecial;
     public Tripulante(String nome, int vida, int stamina , int ataque , int defesa , int velocidade, HabilidadeEspecial habilidadeEspecial){
-        super(vida, stamina, ataque, defesa, velocidade);
+        super(nome, vida, stamina, ataque, defesa, velocidade);
+        this.habilidadeEspecial = habilidadeEspecial;
     }
 
     @Override
@@ -12,4 +14,14 @@ public class Tripulante extends Character {
     @Override
     public void defender() {
     }
+
+    public HabilidadeEspecial getHabilidadeEspecial() {
+        return habilidadeEspecial;
+    }
+
+    public void setHabilidadeEspecial(HabilidadeEspecial habilidadeEspecial) {
+        this.habilidadeEspecial = habilidadeEspecial;
+    }
+
+    // adicao dos get e set ded habilidadeEspecial
 }
