d767527b63c844d7accca073b9874233c83bb509
Eduardo Barros <eduardo.vfbarros@gmail.com>
2026-04-21 13:12:10 -0300

    Criacao dos metodos iniciais e principais para as classes do quiz

--- src/logica/quiz/GerenciadorPergunta.java
+++ src/logica/quiz/GerenciadorPergunta.java
@@ -1,5 +1,19 @@
 package logica.quiz;
-// talvez seja deletado
+import java.util.Random;
+import java.util.ArrayList;
+
 public class GerenciadorPergunta{
+    private ArrayList<Questao> listaQuestoes;
+    private Random sorteio = new Random();
+
+    public GerenciadorPergunta(ArrayList<Questao> listaQuestoes){
+        this.listaQuestoes = listaQuestoes;
+    }   
+
+    public int questaoSorteada(){
+        int tamanhoLista = listaQuestoes.size();
+        return sorteio.nextInt(tamanhoLista);
+    }
+
 
 }
